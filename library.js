1ffc4fc95e214bb478d8474690ebcca3a7b32998
Davis Wainaina <davisjwn@gmail.com>
2014-07-09 15:37:34 +0300

    versioning

--- library.js
+++ library.js
@@ -6,7 +6,7 @@ var request = require('request');
   embed = '<embed type="application/x-shockwave-flash" flashvars="audioUrl=$1" src="https://dl.dropboxusercontent.com/u/17757917/3523697345-audio-player.swf" width="400" height="27" quality="best"></embed>';
   Mixcrate.parse = function (postContent, callback) {
     if (postContent.match(/<a href="(?:https?:\/\/)?(?:www\.)?(?:mixcrate\.com)\/(.+)">.+<\/a>/g)) {
-      var mixUrl = postContent.match(/(?:https?:\/\/)?(?:www\.)?(?:mixcrate\.com)\/(.+)">/g;);
+      var mixUrl = postContent.match(/(?:https?:\/\/)?(?:www\.)?(?:mixcrate\.com)\/(.+)">/g);
       mixUrl = mixUrl[0].replace('">', ''); // too lazy to find a better way to do it.
       request(mixUrl, function (err, response, body) {
         if (!err && response.statusCode === 200) {
