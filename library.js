b87e94bd5d2bd03e0a3c412729cb0bda3db73563
Davis Wainaina <davisjwn@gmail.com>
2014-07-09 16:35:10 +0300

    some stuff doing

--- library.js
+++ library.js
@@ -8,13 +8,19 @@ var mp3Url ="";
   embed = '<embed type="application/x-shockwave-flash" flashvars="audioUrl='+mp3Url+'" src="https://dl.dropboxusercontent.com/u/17757917/3523697345-audio-player.swf" width="400" height="27" quality="best"></embed>';
   Mixcrate.parse = function (postContent, callback) {
     if (postContent.match(/<a href="(?:https?:\/\/)?(?:www\.)?(?:mixcrate\.com)\/(.+)">.+<\/a>/g)) {
+      console.log("Match Found for Mixcrate url");
       var mixUrl = postContent.match(/(?:https?:\/\/)?(?:www\.)?(?:mixcrate\.com)\/(.+)">/g);
       mixUrl = mixUrl[0].replace('">', ''); // too lazy to find a better way to do it.
+      console.log("starting request for "+mixUrl);
       request(mixUrl, function (err, response, body) {
         if (!err && response.statusCode === 200) {
           mp3Url = body.match(/(?:https?:\/\/)?(?:cdn3\.)?(?:mixcrate\.com\/audio)\/(.*)mp3/g)
           postContent = postContent.replace(/<a href="(?:https?:\/\/)?(?:www\.)?(?:mixcrate\.com)\/(.+)">.+<\/a>/g, embed);
-        }
+          console.log('Great Sucess!!');
+        } else {
+          console.log('error: '+ response.statusCode);
+          console.log(body);
+      	}
       });
     }
     callback(null, postContent);
