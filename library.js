5a479d51dbef4e0f917f6c8ef15bf5c390366571
Davis Wainaina <davisjwn@gmail.com>
2014-07-09 15:31:39 +0300

    crushing bugs

--- library.js
+++ library.js
@@ -2,20 +2,15 @@
 var request = require('request');
 
 (function(module) {
-  var Mixcrate = {
-  },
+  var Mixcrate = {},
   embed = '<embed type="application/x-shockwave-flash" flashvars="audioUrl=$1" src="https://dl.dropboxusercontent.com/u/17757917/3523697345-audio-player.swf" width="400" height="27" quality="best"></embed>';
   Mixcrate.parse = function (postContent, callback) {
-    var mixUrlRegex = /<a href="(?:https?:\/\/)?(?:www\.)?(?:mixcrate\.com)\/(.+)">.+<\/a>/g;
-    if (postContent.match(mixUrlRegex)) {
-      var trimUrlRegex = /(?:https?:\/\/)?(?:www\.)?(?:mixcrate\.com)\/(.+)">/g;
-      var mixUrl = postContent.match(trimUrlRegex);
+    if (postContent.match(/<a href="(?:https?:\/\/)?(?:www\.)?(?:mixcrate\.com)\/(.+)">.+<\/a>/g)) {
+      var mixUrl = postContent.match(/(?:https?:\/\/)?(?:www\.)?(?:mixcrate\.com)\/(.+)">/g;);
       mixUrl = mixUrl[0].replace('">', ''); // too lazy to find a better way to do it.
       request(mixUrl, function (err, response, body) {
         if (!err && response.statusCode === 200) {
-          var mp3UrlRegex = /(?:https?:\/\/)?(?:cdn3\.)?(?:mixcrate\.com\/audio)\/(.*)mp3/g;
-          var mp3Url = body.match(mp3UrlRegex);
-          postContent = postContent.replace(mp3Url, embed);
+          postContent = postContent.replace(/(?:https?:\/\/)?(?:cdn3\.)?(?:mixcrate\.com\/audio)\/(.*)mp3/g, embed);
         }
       });
     }
