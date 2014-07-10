ef1b094fbf47f17a2891b91305b1db62da9fda96
Davis Wainaina <davisjwn@gmail.com>
2014-07-10 14:54:19 +0300

    notin

--- library.js
+++ library.js
@@ -1,27 +1,22 @@
 'use strict';
 var request = require('request');
-
-var mp3Url ="";
-
 (function(module) {
-  var Mixcrate = {},
-  embed = '<embed type="application/x-shockwave-flash" flashvars="audioUrl='+mp3Url+'" src="https://dl.dropboxusercontent.com/u/17757917/3523697345-audio-player.swf" width="400" height="27" quality="best"></embed>';
+  var Mixcrate = {};
   Mixcrate.parse = function (postContent, callback) {
-    if (postContent.match(/<a href="(?:https?:\/\/)?(?:www\.)?(?:mixcrate\.com)\/(.+)">.+<\/a>/g)) {
-      console.log("Match Found for Mixcrate url");
-      var mixUrl = postContent.match(/(?:https?:\/\/)?(?:www\.)?(?:mixcrate\.com)\/(.+)">/g);
+    if (postContent.match(/<a href="(?:https?:\/\/)?(?:www\.)?(?:mixcrate\.com)\/(.+)">.+<\/a>/ig)) {
+      var mixUrl = postContent.match(/(?:https?:\/\/)?(?:www\.)?(?:mixcrate\.com)\/(.+)">/ig);
       mixUrl = mixUrl[0].replace('">', ''); // too lazy to find a better way to do it.
-      console.log("starting request for "+mixUrl);
-      request(mixUrl, function (err, response, body) {
+      postContent = postContent.replace(/<a href="(?:https?:\/\/)?(?:www\.)?(?:mixcrate\.com)\/(.+)">.+<\/a>/g,"notin");
+      /*request(mixUrl, function (err, response, body) {
         if (!err && response.statusCode === 200) {
-          mp3Url = body.match(/(?:https?:\/\/)?(?:cdn3\.)?(?:mixcrate\.com\/audio)\/(.*)mp3/g)
+          var mp3Url = body.match(/(?:https?:\/\/)?(?:cdn3\.)?(?:mixcrate\.com\/audio)\/(.*)mp3/g);
+          embed = '<embed type="application/x-shockwave-flash" flashvars="audioUrl='+mp3Url+'" src="https://dl.dropboxusercontent.com/u/17757917/3523697345-audio-player.swf" width="400" height="27" quality="best"></embed>';
           postContent = postContent.replace(/<a href="(?:https?:\/\/)?(?:www\.)?(?:mixcrate\.com)\/(.+)">.+<\/a>/g, embed);
-          console.log('Great Sucess!!');
         } else {
           console.log('error: '+ response.statusCode);
           console.log(body);
       	}
-      });
+      });*/
     }
     callback(null, postContent);
   };
